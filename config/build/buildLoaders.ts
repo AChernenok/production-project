20981e632a422d71b81db579802ad6132090b93a
AChernenok <chernenokalex@gmail.com>
2024-01-07 20:44:58 +0300

    added theme switcher, svg loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,11 @@ import { BuildOptions } from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -23,6 +28,15 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         ],
     }
 
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff?2)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -32,5 +46,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     return [
         typescriptLoader,
         cssLoader,
+        fileLoader,
+        svgLoader
     ]
 }
